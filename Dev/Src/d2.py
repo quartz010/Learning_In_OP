33fa13e54d4fd1faffda04bd9f2b8436879a29e3
林海 陈 <quartz010@outlook.com>
2018-06-04 18:53:37 +0800

    Day 2

---
+++ Dev/Src/d2.py
@@ -0,0 +1,20 @@
+
+passwd = 'admin'
+if 'admin' == passwd:
+    print("authenticated")
+else:
+    print("fatal error")
+
+
+for x in range(1,101):
+    print("fizz"[x%3*4::]+"buzz"[x%5*4::]or x)
+
+for x in range(1,101):
+    if x % 15 == 0:
+        print('fuzzbuzz')
+    elif x % 3 == 0:
+        print('fuzz')
+    elif x % 5 == 0:
+        print('buzz')
+    else:
+        print(x)
